a2c9712a02e0656749746888de457a0e7f662a21
Artyom <aroyanart98@gmail.com>
2025-04-30 23:59:34 +0400

    feat(bank-notification): initial commit of bank-notification service

---
+++ services/bank-notification/src/main/java/am/banking/system/notification/BankNotificationApplication.java
@@ -0,0 +1,11 @@
+package am.banking.system.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankNotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankNotificationApplication.class, args);
+    }
+}
---
+++ services/bank-notification/src/test/java/am/banking/system/notification/BankNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package am.banking.system.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
