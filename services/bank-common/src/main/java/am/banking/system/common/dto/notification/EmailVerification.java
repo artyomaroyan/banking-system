adbe77eda37840141ae3c7afe822c56c90eb32b0
Artyom <aroyanart98@gmail.com>
2025-05-12 23:28:34 +0400

    refactor(bank-common): delete email DTO's

--- services/bank-common/src/main/java/am/banking/system/common/dto/notification/EmailVerification.java
+++
@@ -1,9 +0,0 @@
-package am.banking.system.common.dto.notification;
-
-/**
- * Author: Artyom Aroyan
- * Date: 12.05.25
- * Time: 03:37:45
- */
-public record EmailVerification(String email, String username, String link) {
-}
--- services/bank-common/src/main/java/am/banking/system/common/dto/notification/WelcomeMessage.java
+++
@@ -1,9 +0,0 @@
-package am.banking.system.common.dto.notification;
-
-/**
- * Author: Artyom Aroyan
- * Date: 12.05.25
- * Time: 03:39:00
- */
-public record WelcomeMessage(String email, String username) {
-}
