0aa8846f9724c49ee25f946422c8112c581aab4c
Artyom <aroyanart98@gmail.com>
2025-05-12 23:54:03 +0400

    refactor(bank-common): instead of 3 DTO's make one EmailRequest DTO, move AccountState from security service to common service.

---
+++ services/bank-common/src/main/java/am/banking/system/common/enums/AccountState.java
@@ -0,0 +1,14 @@
+package am.banking.system.common.enums;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 12.05.25
+ * Time: 22:06:17
+ */
+public enum AccountState {
+    PENDING,
+    ACTIVE,
+    LOCKED,
+    INACTIVE,
+    BANNED
+}
