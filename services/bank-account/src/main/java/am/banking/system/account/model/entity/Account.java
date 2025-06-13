2c8d9085f3a96c57767b3679e7c1e2750f3b4477
Artyom <aroyanart98@gmail.com>
2025-06-13 04:11:27 +0400

    fix(bank-account): change @Table annotation to reactive.

--- services/bank-account/src/main/java/am/banking/system/account/model/entity/Account.java
+++ services/bank-account/src/main/java/am/banking/system/account/model/entity/Account.java
@@ -1,36 +1,34 @@
 package am.banking.system.account.model.entity;
 
 import am.banking.system.account.model.enums.AccountType;
+import am.banking.system.common.entity.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 /**
  * Author: Artyom Aroyan
  * Date: 01.06.25
  * Time: 14:37:27
  */
+@Table
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Account implements Serializable {
+public class Account extends BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    @Id
-    private Long id;
-    private String accountOwnerId;
+    private Long accountOwnerId;
     private String accountNumber;
     private String accountOwnerUsername;
     private String accountOwnerFullName;
     private String accountOwnerEmail;
     private BigDecimal balance;
     private AccountType accountType;
-    private LocalDate createdAt;
 }
