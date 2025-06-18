7252a02f806d4269feabc6d51c2b1ffc35f33bfc
Artyom <aroyanart98@gmail.com>
2025-06-18 23:55:37 +0400

    refactor(account): make entity filed immutable

--- services/bank-account/src/main/java/am/banking/system/account/model/entity/Account.java
+++ services/bank-account/src/main/java/am/banking/system/account/model/entity/Account.java
@@ -4,7 +4,6 @@ import am.banking.system.account.model.enums.AccountType;
 import am.banking.system.common.entity.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serial;
@@ -16,19 +15,18 @@ import java.math.BigDecimal;
  * Date: 01.06.25
  * Time: 14:37:27
  */
+@Table
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "account")
 public class Account extends BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private Long accountOwnerId;
-    private String accountNumber;
-    private String accountOwnerUsername;
-    private String accountOwnerFullName;
-    private String accountOwnerEmail;
-    private BigDecimal balance;
-    private AccountType accountType;
+    private final Integer accountOwnerId;
+    private final String accountNumber;
+    private final String accountOwnerUsername;
+    private final String accountOwnerFullName;
+    private final String accountOwnerEmail;
+    private final BigDecimal balance;
+    private final AccountType accountType;
 }
