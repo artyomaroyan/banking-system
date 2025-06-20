1e186d92ee37977709cb8cc1dd26f074d026912b
Artyom <aroyanart98@gmail.com>
2025-06-20 22:06:12 +0400

    feat(user): add custom repositories and reactive DTO mapper for role-permission resolution
    - Added `RolePermissionCustomRepository` and `UserRoleCustomRepository` along with their implementation classes to support custom data fetching for mapper.
    - Introduced `ReactiveMapper` interface and `UserDtoMapper` implementation to resolve DTO mapping issues between User → Role → Permission.
    - Mapping issues have been resolved; current mapping flow works as expected.

---
+++ services/bank-user/src/main/java/am/banking/system/user/infrastructure/repository/implementation/UserRoleRepositoryImpl.java
@@ -0,0 +1,31 @@
+package am.banking.system.user.infrastructure.repository.implementation;
+
+import am.banking.system.user.model.repository.UserRoleCustomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 20.06.25
+ * Time: 21:57:44
+ */
+@Repository
+@RequiredArgsConstructor
+public class UserRoleRepositoryImpl implements UserRoleCustomRepository {
+    private final DatabaseClient databaseClient;
+
+    @Override
+    public Flux<String> findRolesByUserId(Integer userId) {
+        return databaseClient.sql("""
+                        SELECT r.role_name
+                        FROM user_db.usr.user_role ur
+                        JOIN user_db.usr.role r ON ur.role_id = r.id
+                        WHERE ur.user_id = :userId
+                        """)
+                .bind("userId", userId)
+                .map(row -> row.get("role_name", String.class))
+                .all();
+    }
+}
---
+++ services/bank-user/src/main/java/am/banking/system/user/mapper/reactive/UserDtoMapper.java
@@ -0,0 +1,41 @@
+package am.banking.system.user.mapper.reactive;
+
+import am.banking.system.common.dto.user.UserDto;
+import am.banking.system.user.model.entity.User;
+import am.banking.system.user.model.repository.RolePermissionRepository;
+import am.banking.system.user.model.repository.UserRoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.stream.Collectors;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 20.06.25
+ * Time: 18:35:43
+ */
+@Component
+@RequiredArgsConstructor
+public class UserDtoMapper implements ReactiveMapper<User, UserDto> {
+    private final UserRoleRepository userRoleRepository;
+    private final RolePermissionRepository rolePermissionRepository;
+
+    @Override
+    public Mono<UserDto> map(User source) {
+        return userRoleRepository.findRolesByUserId(source.getId())
+                .collect(Collectors.toSet())
+                .flatMap(roles -> Flux.fromIterable(roles)
+                        .flatMap(rolePermissionRepository::findRolePermissionsByRoleName)
+                        .collect(Collectors.toSet())
+                        .map(permissions -> new UserDto(
+                                source.getId(),
+                                source.getUsername(),
+                                source.getPassword(),
+                                source.getEmail(),
+                                roles,
+                                permissions
+                        )));
+    }
+}
