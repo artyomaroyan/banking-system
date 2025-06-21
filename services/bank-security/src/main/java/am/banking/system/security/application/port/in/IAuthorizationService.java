3ed48d40658dd53e8624ff3a28af18a8ff14fc4e
Artyom <aroyanart98@gmail.com>
2025-06-21 19:07:25 +0400

    feat(security): create `IAuthorizeService` class

---
+++ services/bank-security/src/main/java/am/banking/system/security/application/port/in/IAuthorizationService.java
@@ -0,0 +1,12 @@
+package am.banking.system.security.application.port.in;
+
+import am.banking.system.common.shared.enums.PermissionEnum;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 21.06.25
+ * Time: 18:27:26
+ */
+public interface IAuthorizationService {
+    boolean isAuthorized(String token, PermissionEnum permission);
+}
