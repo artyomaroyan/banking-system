8706d3453c6d391642522bdfbc022657006dfb93
Artyom <aroyanart98@gmail.com>
2025-06-29 18:33:10 +0400

    feat(notification): add Kafka error handler for improved message processing reliability
    - Implemented a Kafka error handler to catch and manage exceptions during message consumption.
    - Enhances fault tolerance and observability in the notification pipeline.

---
+++ services/bank-notification/src/main/java/am/banking/system/notification/infrastructure/kafka/KafkaConsumingConfiguration.java
@@ -0,0 +1,23 @@
+package am.banking.system.notification.infrastructure.kafka;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 29.06.25
+ * Time: 18:20:59
+ */
+@Configuration
+public class KafkaConsumingConfiguration {
+
+    @Bean
+    public DefaultErrorHandler kafkaErrorHandler(KafkaTemplate<String, Object> kafkaTemplate) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 2L));
+    }
+}
