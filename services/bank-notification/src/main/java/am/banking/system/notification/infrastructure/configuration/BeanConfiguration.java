0b7e259b5c79d79bff1fe6ccf74e0e0d21244287
Artyom <aroyanart98@gmail.com>
2025-07-01 23:44:03 +0400

    fix(notification): temporarily relocate and correct mail configuration
    - Moved mail-related configurations from the config server to the notification service's local `application.yml` for temporary use.
    - Fixed misconfigured or incomplete mail properties to ensure proper SMTP setup.

--- services/bank-notification/src/main/java/am/banking/system/notification/infrastructure/configuration/BeanConfiguration.java
+++ services/bank-notification/src/main/java/am/banking/system/notification/infrastructure/configuration/BeanConfiguration.java
@@ -1,10 +1,13 @@
 package am.banking.system.notification.infrastructure.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 /**
  * Author: Artyom Aroyan
@@ -12,10 +15,34 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
  * Time: 00:16:29
  */
 @Configuration
-//@EnableConfigurationProperties(NotificationProperties.class)
+@RequiredArgsConstructor
+//@EnableConfigurationProperties(EmailConfiguration.class)
 public class BeanConfiguration {
+    private final EmailConfiguration mailConfiguration;
+
     @Bean
     protected JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl();
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(mailConfiguration.getHost());
+        mailSender.setPort(mailConfiguration.getPort());
+        mailSender.setUsername(mailConfiguration.getUsername());
+        mailSender.setPassword(mailConfiguration.getPassword());
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.smtp.auth", mailConfiguration.isAuth());
+        properties.put("mail.smtp.starttls.enable", mailConfiguration.isEnable());
+        properties.put("mail.smtp.starttls.required", mailConfiguration.isRequire());
+        properties.put("mail.transport.protocol", mailConfiguration.getProtocol());
+        mailSender.setJavaMailProperties(properties);
+        return mailSender;
+
+
+//        Properties properties = mailSender.getJavaMailProperties();
+//        properties.put("mail.smtp.auth", mailConfiguration.getProperties().getSmtp().isAuth());
+//        properties.put("mail.smtp.starttls.enable", mailConfiguration.getProperties().getSmtp().getStartTls().isEnable());
+//        properties.put("mail.smtp.starttls.required", mailConfiguration.getProperties().getSmtp().getStartTls().isRequire());
+//        properties.put("mail.transport.protocol", mailConfiguration.getProtocol());
+//        mailSender.setJavaMailProperties(properties);
+//        return mailSender;
     }
 }
