7cb31f774a2c1efafd3540cfc5cd1fdf46520c5f
Artyom <aroyanart98@gmail.com>
2025-07-23 16:45:47 +0400

    feat(account): add custom mapping configuration and backup of removed code
    - Introduced custom account mapping configuration to handle complex mapping scenarios.
    - Added `deleted-code.txt` file to temporarily store removed or deprecated code for reference.

---
+++ services/bank-account/src/main/java/am/banking/system/account/application/configuration/AccountMappingConfiguration.java
@@ -0,0 +1,52 @@
+package am.banking.system.account.application.configuration;
+
+import am.banking.system.account.api.dto.AccountRequest;
+import am.banking.system.account.domain.entity.Account;
+import am.banking.system.common.shared.dto.account.AccountResponse;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 22.07.25
+ * Time: 01:15:24
+ */
+@Configuration
+@RequiredArgsConstructor
+public class AccountMappingConfiguration {
+    private final ModelMapper modelMapper;
+
+    @PostConstruct
+    public void setupRequestMapping() {
+        modelMapper.createTypeMap(AccountRequest.class, Account.class)
+                .setProvider(ctx -> {
+                    AccountRequest request = (AccountRequest) ctx.getSource();
+                    return new Account(
+                            request.accountOwnerId(),
+                            request.accountNumber(),
+                            request.accountOwnerUsername(),
+                            request.balance(),
+                            request.accountType(),
+                            request.accountCurrency()
+                    );
+                });
+    }
+
+    @PostConstruct
+    public void setupResponseMapping() {
+        modelMapper.createTypeMap(Account.class, AccountResponse.class)
+                .setProvider(ctx -> {
+                    Account account = (Account) ctx.getSource();
+                    return new AccountResponse(
+                            account.getAccountOwnerId(),
+                            account.getAccountNumber(),
+                            account.getAccountOwnerUsername(),
+                            account.getBalance(),
+                            account.getAccountType(),
+                            account.getCreatedAt()
+                    );
+                });
+    }
+}
