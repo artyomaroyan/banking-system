36534e5939f433fd0568e2ba405dce1edaa83863
Artyom <aroyanart98@gmail.com>
2025-08-01 23:21:02 +0400

    refactor(account): migrate entity IDs to UUID and clean up unused code
    - Updated all entity `id` fields to use `UUID` instead of other types for improved consistency and scalability.
    - Reflected ID changes in corresponding Flyway migration files.
    - Removed unused objects and code for better maintainability.

--- services/bank-account/src/main/java/am/banking/system/account/domain/repository/AccountRepository.java
+++ services/bank-account/src/main/java/am/banking/system/account/domain/repository/AccountRepository.java
@@ -5,12 +5,14 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 /**
  * Author: Artyom Aroyan
  * Date: 01.06.25
  * Time: 15:28:12
  */
 @Repository
-public interface AccountRepository extends ReactiveCrudRepository<Account, String> {
+public interface AccountRepository extends ReactiveCrudRepository<Account, UUID> {
     Mono<Boolean> existsAccountsByAccountNumber(String accountNumber);
 }
