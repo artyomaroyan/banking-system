d072650e878eb78cd3a2c23c491afcebfce4fcb8
Artyom <aroyanart98@gmail.com>
2025-08-01 23:22:51 +0400

    refactor(common): migrate entity IDs to UUID and remove unused code
    - Changed all entity `id` fields to use `UUID` for consistency across the system.
    - Removed unused objects and classes to improve code clarity and maintainability.

--- services/bank-notification/src/main/java/am/banking/system/notification/domain/repository/NotificationRepository.java
+++ services/bank-notification/src/main/java/am/banking/system/notification/domain/repository/NotificationRepository.java
@@ -4,11 +4,13 @@ import am.banking.system.notification.domain.entity.Notification;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 /**
  * Author: Artyom Aroyan
  * Date: 01.05.25
  * Time: 00:47:23
  */
 @Repository
-public interface NotificationRepository extends ReactiveCrudRepository<Notification, Integer> {
+public interface NotificationRepository extends ReactiveCrudRepository<Notification, UUID> {
 }
