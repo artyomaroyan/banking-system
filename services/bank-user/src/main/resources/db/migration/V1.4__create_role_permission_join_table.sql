1703f3312f482c3052e4f891f28d0f124d5cdda8
Artyom <aroyanart98@gmail.com>
2025-08-02 23:35:17 +0400

    refactor(user): update user_role and role_permission migration files
    - Modified Flyway migration scripts for `user_role` and `role_permission` tables to reflect schema or data structure improvements.

--- services/bank-user/src/main/resources/db/migration/V1.4__create_role_permission_join_table.sql
+++ services/bank-user/src/main/resources/db/migration/V1.4__create_role_permission_join_table.sql
@@ -1,5 +1,7 @@
+create extension if not exists pgcrypto;
+
 create table if not exists usr.role_permission(
+    id uuid unique not null primary key default gen_random_uuid() ,
     role_id uuid not null references user_db.usr.role(id) on delete cascade ,
-    permission_id uuid not null references user_db.usr.permission(id) on delete cascade,
-    primary key (role_id, permission_id)
+    permission_id uuid not null references user_db.usr.permission(id) on delete cascade
 );
