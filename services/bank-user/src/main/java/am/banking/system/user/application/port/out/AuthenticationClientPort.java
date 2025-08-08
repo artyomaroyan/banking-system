879357d17e50b23485fd11294d8ee0b2231908af
Artyom <aroyanart98@gmail.com>
2025-08-08 23:46:23 +0400

    feat(user): add authentication client port and implementation
    - Introduced `AuthenticationClientPort` interface for authentication service communication.
    - Added `AuthenticationClient` implementation to handle authentication requests.

---
+++ services/bank-user/src/main/java/am/banking/system/user/application/port/out/AuthenticationClientPort.java
@@ -0,0 +1,13 @@
+package am.banking.system.user.application.port.out;
+
+import am.banking.system.common.shared.dto.security.AuthenticationResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 08.08.25
+ * Time: 23:28:28
+ */
+public interface AuthenticationClientPort {
+    Mono<AuthenticationResponse> authenticate(String username, String password);
+}
---
+++ services/bank-user/src/main/java/am/banking/system/user/infrastructure/adapter/out/security/AuthenticationClient.java
@@ -0,0 +1,52 @@
+package am.banking.system.user.infrastructure.adapter.out.security;
+
+import am.banking.system.common.shared.dto.security.AuthenticationResponse;
+import am.banking.system.common.shared.response.WebClientResponseHandler;
+import am.banking.system.user.api.dto.AuthenticationRequest;
+import am.banking.system.user.application.port.out.AuthenticationClientPort;
+import am.banking.system.user.application.port.out.UserTokenClientPort;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 08.08.25
+ * Time: 23:31:51
+ */
+@Slf4j
+@Service
+public class AuthenticationClient implements AuthenticationClientPort {
+    private final WebClient webClient;
+    private final UserTokenClientPort userTokenClient;
+    private final WebClientResponseHandler webClientResponseHandler;
+
+    public AuthenticationClient(@Qualifier("securityWebClient") WebClient webClient,
+                                UserTokenClientPort userTokenClient,
+                                WebClientResponseHandler webClientResponseHandler) {
+        this.webClient = webClient;
+        this.userTokenClient = userTokenClient;
+        this.webClientResponseHandler = webClientResponseHandler;
+    }
+
+    @Override
+    public Mono<AuthenticationResponse> authenticate(String username, String password) {
+        return userTokenClient.generateSystemToken()
+                .flatMap(systemToken -> webClient.post()
+                        .uri("/api/internal/security/authenticate")
+                        .header(AUTHORIZATION, "Bearer " + systemToken)
+                        .contentType(APPLICATION_JSON)
+                        .bodyValue(new AuthenticationRequest(username, password))
+                        .exchangeToMono(response -> webClientResponseHandler
+                                .response(response, AuthenticationResponse.class, "Authentication"))
+                        .timeout(Duration.ofSeconds(5))
+                );
+    }
+}
