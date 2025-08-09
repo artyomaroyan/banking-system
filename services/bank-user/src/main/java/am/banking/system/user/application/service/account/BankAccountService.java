17525b9c14f3c57a5a421f78af83175b6ecb093e
Artyom <aroyanart98@gmail.com>
2025-08-09 23:26:46 +0400

    feat(user): add UserAuthenticationService and BankAccountService
    - Implemented `UserAuthenticationService` for handling user authentication logic.
    - Added `BankAccountService` to manage bank account operations.

---
+++ services/bank-user/src/main/java/am/banking/system/user/application/service/account/BankAccountService.java
@@ -0,0 +1,26 @@
+package am.banking.system.user.application.service.account;
+
+import am.banking.system.user.application.port.in.account.BankAccountUserCase;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 09.08.25
+ * Time: 00:05:27
+ */
+@Service
+public class BankAccountService implements BankAccountUserCase {
+
+    @Override
+    public Mono<String> getAccountNumber() {
+        return null;
+    }
+
+    @Override
+    public Mono<BigDecimal> getAccountBalance() {
+        return null;
+    }
+}
---
+++ services/bank-user/src/main/java/am/banking/system/user/application/service/auth/UserAuthenticationService.java
@@ -0,0 +1,46 @@
+package am.banking.system.user.application.service.auth;
+
+import am.banking.system.common.shared.dto.security.AuthenticationRequest;
+import am.banking.system.common.shared.dto.security.AuthenticationResponse;
+import am.banking.system.common.shared.response.Result;
+import am.banking.system.user.application.port.in.user.UserAuthenticationUseCase;
+import am.banking.system.user.application.port.out.AuthenticationClientPort;
+import am.banking.system.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 08.08.25
+ * Time: 23:48:04
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserAuthenticationService implements UserAuthenticationUseCase {
+    private final UserRepository userRepository;
+    private final AuthenticationClientPort authenticationClient;
+
+    @Override
+    public Mono<Result<AuthenticationResponse>> authenticate(AuthenticationRequest request) {
+        return userRepository.existsByUsername(request.username())
+                .flatMap(exists -> {
+                    if (!exists) {
+                        log.info("User '{}' does not exist", request.username());
+                        return Mono.just(Result.error("User does not exists:", NOT_FOUND.value()));
+                    }
+
+                    return authenticationClient.authenticate(request.username(), request.password())
+                            .map(Result.success("Successfully authenticated"))
+                            .onErrorResume(ex -> {
+                                log.warn("Invalid credentials: {}", ex.getMessage());
+                                return Mono.just(Result.error("Invalid credentials", BAD_REQUEST.value()));
+                            });
+                });
+    }
+}
