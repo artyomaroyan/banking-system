e57b25cc2ba467e733f4a2932d865affb172dc45
Artyom <aroyanart98@gmail.com>
2025-08-13 22:00:43 +0400

    feat(gateway):
    - add GatewaySecurityConfiguration class.

---
+++ services/api-gateway/src/main/java/am/banking/system/apigateway/configuration/GatewaySecurityConfiguration.java
@@ -0,0 +1,12 @@
+package am.banking.system.apigateway.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 13.08.25
+ * Time: 21:34:43
+ */
+@Configuration
+public class GatewaySecurityConfiguration {
+}
