c09750541d4bfc355e97b6c18a504b7538396e32
Artyom <aroyanart98@gmail.com>
2025-08-14 01:28:39 +0400

    feat(gateway):
    - recreate api-gateway service.

---
+++ services/api-gateway/src/test/java/am/banking/system/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package am.banking.system.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
