410a74dd721865f2b043dd6eb6366a11c089d56a
Artyom <aroyanart98@gmail.com>
2025-08-17 01:08:25 +0400

    refactor(user): finalize user update method with new return type
    - Change user update method return type from `Mono<Result<UserResponse>>`
      to `Mono<Result<String>>` to simplify response handling.
    - Complete the implementation of the update method with proper validation
      and persistence logic.
    - let's test it...

--- services/bank-user/src/main/java/am/banking/system/user/application/port/in/user/UserUpdateUseCase.java
+++ services/bank-user/src/main/java/am/banking/system/user/application/port/in/user/UserUpdateUseCase.java
@@ -2,7 +2,6 @@ package am.banking.system.user.application.port.in.user;
 
 import am.banking.system.common.shared.response.Result;
 import am.banking.system.user.api.dto.UserRequest;
-import am.banking.system.user.api.dto.UserResponse;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -13,5 +12,5 @@ import java.util.UUID;
  * Time: 22:40:31
  */
 public interface UserUpdateUseCase {
-    Mono<Result<UserResponse>> update(UUID userId, UserRequest request);
+    Mono<Result<String>> update(UUID userId, UserRequest request);
 }
--- services/bank-user/src/main/java/am/banking/system/user/application/service/user/UserUpdateService.java
+++ services/bank-user/src/main/java/am/banking/system/user/application/service/user/UserUpdateService.java
@@ -1,13 +1,16 @@
 package am.banking.system.user.application.service.user;
 
+import am.banking.system.common.shared.dto.user.UserDto;
 import am.banking.system.common.shared.exception.NotFoundException;
 import am.banking.system.common.shared.response.Result;
 import am.banking.system.user.api.dto.UserRequest;
-import am.banking.system.user.api.dto.UserResponse;
+import am.banking.system.user.application.mapper.ReactiveMapper;
 import am.banking.system.user.application.port.in.user.UserFactoryUseCase;
 import am.banking.system.user.application.port.in.user.UserUpdateUseCase;
 import am.banking.system.user.application.service.validation.RequestValidation;
+import am.banking.system.user.domain.entity.User;
 import am.banking.system.user.domain.repository.UserRepository;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,8 +18,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.*;
 
 /**
  * Author: Artyom Aroyan
@@ -29,32 +31,49 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 public class UserUpdateService implements UserUpdateUseCase {
     private final UserRepository userRepository;
     private final UserFactoryUseCase userFactory;
+    private final ReactiveMapper<User, UserDto> userReactiveMapper;
     private final RequestValidation<UserRequest> requestValidation;
 
     @Override
-    public Mono<Result<UserResponse>> update(UUID userId, UserRequest request) {
-        var userFromDb = userRepository.findById(userId).block();
+    public Mono<Result<String>> update(UUID userId, UserRequest request) {
+        return validateRequest(request)
+                .flatMap(validRequest -> processUserUpdate(userId, validRequest))
+                .onErrorResume(this::handleRegistrationError);
+    }
 
-        return userRepository.findById(userFromDb.getId())
-                .switchIfEmpty(Mono.defer(() -> {
-                    log.warn("No user found with id {}", userId);
-                    return Mono.defer(() -> new NotFoundException("No user found with id " + userId));
-                }));
-
+    private Mono<UserRequest> validateRequest(UserRequest request) {
         return requestValidation.isValidRequest(request)
                 .flatMap(errors -> {
                     if (!errors.message().isEmpty()) {
-                        String errorMessage = String.join(", ", errors.message());
-                        log.warn("Invalid request received: {}", errorMessage);
-                        return Mono.just(Result.error(errorMessage, BAD_REQUEST.value()));
+                        String errorMessage = String.join(" ", errors.message());
+                        log.error("Validation failed for {}: {}",  request.email(), errorMessage);
+                        return Mono.error(new ValidationException(errorMessage));
                     }
-
-                    return userFactory.updateUser(userId, request)
-                            .doOnNext(user -> log.info("user updated: {}", user))
-                            .onErrorResume(error -> {
-                                log.error("Could not update user: {}", error.getMessage(), error);
-                                return Mono.just(Result.error("Update failed. please try again later", INTERNAL_SERVER_ERROR.value()));
-                            });
+                    return Mono.just(request);
                 });
+    }
+
+    private Mono<Result<String>> processUserUpdate(UUID userId, UserRequest request) {
+        return userRepository.findById(userId)
+                .switchIfEmpty(Mono.error(new NotFoundException("User not found with id:" + userId)))
+                .flatMap(existingUser -> userFactory.updateUser(existingUser, request))
+                .flatMap(userReactiveMapper::map)
+                .thenReturn(Result.success("Data successfully updated"))
+                .onErrorResume(NotFoundException.class, ex -> {
+                    log.warn(ex.getMessage());
+                    return Mono.just(Result.error(ex.getMessage(), NOT_FOUND.value()));
+                })
+                .onErrorResume(ex -> {
+                    log.warn("Update failed for user {}: {}", userId, ex.getMessage(), ex);
+                    return Mono.just(Result.error("Update failed. Please try again later.", INTERNAL_SERVER_ERROR.value()));
+                });
+    }
+
+    private Mono<Result<String>> handleRegistrationError(Throwable error) {
+        log.error("Data update failed: {}", error.getMessage(), error);
+        if (error instanceof ValidationException) {
+            return Mono.just(Result.error(error.getMessage(), BAD_REQUEST.value()));
+        }
+        return Mono.just(Result.error("Update process failed. Please try again later.", INTERNAL_SERVER_ERROR.value()));
     }
 }
