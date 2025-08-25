dab5cdaf15fbac5112b474222ad2e2f5963f1237
Artyom <aroyanart98@gmail.com>
2025-08-26 03:13:34 +0400

    feat(transaction): just add `UserTransferRequestedConsumer` class.

---
+++ services/transaction-service/src/main/java/am/banking/system/transaction/consumer/UserTransferRequestedConsumer.java
@@ -0,0 +1,14 @@
+package am.banking.system.transaction.consumer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 26.08.25
+ * Time: 03:05:58
+ */
+@Component
+@RequiredArgsConstructor
+public class UserTransferRequestedConsumer {
+}
