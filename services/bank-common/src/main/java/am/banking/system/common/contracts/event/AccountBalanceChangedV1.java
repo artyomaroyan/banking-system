9ce65a66fc0d51ac2e90b62fc35404fdeb91dd12
Artyom <aroyanart98@gmail.com>
2025-08-26 03:11:28 +0400

    refactor(common): clean up callbacks and improve event record
    - Temporarily comment out unused `BeforeConvertCallback` and `BaseEntityCallback` objects.
    - Improve `AccountBalanceChangedV1` record for better clarity and consistency.

--- services/bank-common/src/main/java/am/banking/system/common/contracts/event/AccountBalanceChangedV1.java
+++ services/bank-common/src/main/java/am/banking/system/common/contracts/event/AccountBalanceChangedV1.java
@@ -1,5 +1,11 @@
 package am.banking.system.common.contracts.event;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.validation.annotation.Validated;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
@@ -9,11 +15,18 @@ import java.util.UUID;
  * Date: 23.08.25
  * Time: 01:20:23
  */
-public record AccountBalanceChangedV1(
-        UUID eventId,
-        UUID accountId,
-        BigDecimal newBalance,
-        long version,
-        Instant occurredAt
-) {
+@Validated
+public record AccountBalanceChangedV1(@NotNull UUID eventId, @NotNull UUID accountId, @NotNull @Positive BigDecimal newBalance,
+                                      @NotNull long version, @NotNull Instant occurredAt) implements DomainEvent {
+
+    @JsonCreator
+    public AccountBalanceChangedV1(@JsonProperty("eventId") UUID eventId, @JsonProperty("accountId") UUID accountId,
+                                   @JsonProperty("newBalance") BigDecimal newBalance, @JsonProperty("version") long version,
+                                   @JsonProperty("occurredAt") Instant occurredAt) {
+        this.eventId = eventId;
+        this.accountId = accountId;
+        this.newBalance = newBalance;
+        this.version = version;
+        this.occurredAt = occurredAt;
+    }
 }
