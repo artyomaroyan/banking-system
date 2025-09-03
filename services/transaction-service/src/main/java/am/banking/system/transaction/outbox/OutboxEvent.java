562df84c242554fc88ad942a0ad2e4f31e87be28
Artyom <aroyanart98@gmail.com>
2025-09-03 21:37:03 +0400

    refactor(transaction): update outbox entity change some fields.

--- services/transaction-service/src/main/java/am/banking/system/transaction/outbox/OutboxEvent.java
+++ services/transaction-service/src/main/java/am/banking/system/transaction/outbox/OutboxEvent.java
@@ -1,9 +1,8 @@
 package am.banking.system.transaction.outbox;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
@@ -17,32 +16,33 @@ import java.util.UUID;
  * Time: 01:21:26
  */
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Table("transaction.outbox_event")
 public class OutboxEvent {
     @Id
-    private final UUID eventId;
-    private final UUID aggregateId;
-    private final String aggregateType;
-    private final String type;
-    private final String payload;
-    private final Instant occurredAt;
-    @Setter
+    private UUID id;
+    private String topic;
+    private String key;
+    private String payload;
+    private String header;
+    private Instant createdAt;
     private boolean published;
 
-    public static OutboxEvent from(String aggregateType, UUID aggregateId, String type, Object payload, ObjectMapper mapper) {
-        try {
-            return new OutboxEvent(
-                    UUID.randomUUID(),
-                    aggregateId,
-                    aggregateType,
-                    type,
-                    mapper.writeValueAsString(payload),
-                    Instant.now(),
-                    false
-            );
-        } catch (JsonProcessingException ex) {
-            throw new RuntimeException("Failed to serialize event", ex);
-        }
-    }
+//    public static OutboxEvent from(String aggregateType, UUID aggregateId, String type, Object payload, ObjectMapper mapper) {
+//        try {
+//            return new OutboxEvent(
+//                    UUID.randomUUID(),
+//                    aggregateId,
+//                    aggregateType,
+//                    type,
+//                    mapper.writeValueAsString(payload),
+//                    Instant.now(),
+//                    false
+//            );
+//        } catch (JsonProcessingException ex) {
+//            throw new RuntimeException("Failed to serialize event", ex);
+//        }
+//    }
 }
