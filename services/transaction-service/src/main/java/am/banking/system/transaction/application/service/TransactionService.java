6184e857974c1ea9f494ecb273b9b2ed1587959e
Artyom <aroyanart98@gmail.com>
2025-09-03 21:39:02 +0400

    refactor(transaction):
    - temporary comment `TransferService` class.
    - implement `createTransaction` logic in `TransactionService`.

--- services/transaction-service/src/main/java/am/banking/system/transaction/application/service/TransactionService.java
+++ services/transaction-service/src/main/java/am/banking/system/transaction/application/service/TransactionService.java
@@ -1,11 +1,21 @@
 package am.banking.system.transaction.application.service;
 
+import am.banking.system.common.messages.ValidateAndReserveCommand;
 import am.banking.system.transaction.api.dto.TransactionRequest;
 import am.banking.system.transaction.application.port.in.TransactionUseCase;
+import am.banking.system.transaction.domain.model.Transaction;
+import am.banking.system.transaction.domain.repository.TransactionRepository;
+import am.banking.system.transaction.outbox.OutboxEvent;
+import am.banking.system.transaction.outbox.OutboxRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
 
 /**
  * Author: Artyom Aroyan
@@ -16,6 +26,9 @@ import reactor.core.publisher.Mono;
 @Service
 @RequiredArgsConstructor
 public class TransactionService implements TransactionUseCase {
+    private final ObjectMapper objectMapper;
+    private final OutboxRepository outboxRepository;
+    private final TransactionRepository transactionRepository;
 
     @Override
     public Mono<Void> transferToCard(TransactionRequest request) {
@@ -25,5 +38,44 @@ public class TransactionService implements TransactionUseCase {
     @Override
     public Mono<Void> transferToAccount(TransactionRequest request) {
         return null;
+    }
+
+    private Mono<Transaction> createTransaction(UUID userId, UUID transferId, String from, String to, String amountStr, String idempotencyKey) {
+        BigDecimal amount = new BigDecimal(amountStr);
+        Mono<Transaction> existing = Mono.empty();
+        if (idempotencyKey != null) {
+            existing = transactionRepository.findByIdempotencyKey(idempotencyKey);
+        }
+
+        return existing.switchIfEmpty(Mono.defer(() -> {
+            var entity = Transaction.builder()
+                    .userId(userId)
+                    .debitAccount(from)
+                    .creditAccount(to)
+                    .amount(amount)
+                    .status("NEW")
+                    .idempotencyKey(idempotencyKey)
+                    .build();
+
+            return transactionRepository.save(entity)
+                    .flatMap(saved -> {
+                        var command = new ValidateAndReserveCommand(transferId, from, amount, Instant.now());
+
+                        try {
+                            var payload = objectMapper.writeValueAsString(command);
+                            var outbox = new OutboxEvent();
+                            outbox.setId(UUID.randomUUID());
+                            outbox.setTopic("accounts.commands");
+                            outbox.setKey(from);
+                            outbox.setPayload(payload);
+                            outbox.setHeader("{\"correlationId\":\"" + transferId + "\"}");
+                            outbox.setPublished(false);
+                            outbox.setCreatedAt(Instant.now());
+                            return outboxRepository.save(outbox).thenReturn(saved);
+                        } catch (Exception e) {
+                            return Mono.error(e);
+                        }
+                    });
+        }));
     }
 }
--- services/transaction-service/src/main/java/am/banking/system/transaction/application/service/TransferService.java
+++ services/transaction-service/src/main/java/am/banking/system/transaction/application/service/TransferService.java
@@ -1,31 +1,31 @@
-package am.banking.system.transaction.application.service;
-
-import am.banking.system.common.contracts.event.UserTransferRequestedV1;
-import am.banking.system.transaction.outbox.OutboxEvent;
-import am.banking.system.transaction.outbox.OutboxRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-/**
- * Author: Artyom Aroyan
- * Date: 27.08.25
- * Time: 01:45:51
- */
-@Service
-@RequiredArgsConstructor
-public class TransferService {
-    private final ObjectMapper objectMapper;
-    private final OutboxRepository outboxRepository;
-
-    public void handleTransfer(UserTransferRequestedV1 requestedV1) {
-        OutboxEvent event = OutboxEvent.from(
-                "Transaction",
-                requestedV1.userId(),
-                "UserTransferRequestedV1",
-                requestedV1,
-                objectMapper
-        );
-        outboxRepository.save(event).subscribe();
-    }
-}
+//package am.banking.system.transaction.application.service;
+//
+//import am.banking.system.common.contracts.event.UserTransferRequestedV1;
+//import am.banking.system.transaction.outbox.OutboxEvent;
+//import am.banking.system.transaction.outbox.OutboxRepository;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * Author: Artyom Aroyan
+// * Date: 27.08.25
+// * Time: 01:45:51
+// */
+//@Service
+//@RequiredArgsConstructor
+//public class TransferService {
+//    private final ObjectMapper objectMapper;
+//    private final OutboxRepository outboxRepository;
+//
+//    public void handleTransfer(UserTransferRequestedV1 requestedV1) {
+//        OutboxEvent event = OutboxEvent.from(
+//                "Transaction",
+//                requestedV1.userId(),
+//                "UserTransferRequestedV1",
+//                requestedV1,
+//                objectMapper
+//        );
+//        outboxRepository.save(event).subscribe();
+//    }
+//}
