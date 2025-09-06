905a3a79a8db3073ca5fbad05fbaa6ad46b7c937
Artyom <aroyanart98@gmail.com>
2025-09-06 23:39:55 +0400

    refactor(user): implement `TransactionClient's` `createTransfer` method.

--- services/bank-user/src/main/java/am/banking/system/user/application/port/out/transaction/TransactionClientPort.java
+++ services/bank-user/src/main/java/am/banking/system/user/application/port/out/transaction/TransactionClientPort.java
@@ -1,9 +1,18 @@
 package am.banking.system.user.application.port.out.transaction;
 
+import am.banking.system.common.shared.dto.transaction.TransactionRequest;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
 /**
  * Author: Artyom Aroyan
  * Date: 31.08.25
  * Time: 16:47:56
  */
 public interface TransactionClientPort {
+    Mono<Map<String, String>> createTransfer(@RequestBody TransactionRequest request,
+                                            @RequestHeader(value = "Idempotency-Key", required = false) String idempotencyKey);
 }
--- services/bank-user/src/main/java/am/banking/system/user/infrastructure/adapter/out/transaction/TransactionClient.java
+++ services/bank-user/src/main/java/am/banking/system/user/infrastructure/adapter/out/transaction/TransactionClient.java
@@ -1,12 +1,22 @@
 package am.banking.system.user.infrastructure.adapter.out.transaction;
 
+import am.banking.system.common.shared.dto.transaction.TransactionRequest;
 import am.banking.system.common.shared.response.WebClientResponseHandler;
 import am.banking.system.user.application.port.out.security.UserTokenClientPort;
 import am.banking.system.user.application.port.out.transaction.TransactionClientPort;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Author: Artyom Aroyan
@@ -27,5 +37,27 @@ public class TransactionClient implements TransactionClientPort {
         this.webClientResponseHandler = webClientResponseHandler;
     }
 
-
+    @Override
+    @Retry(name = "transactionService")
+    @CircuitBreaker(name = "transactionService")
+    public Mono<Map<String, String>> createTransfer(TransactionRequest request, String idempotencyKey) {
+        UUID transactionId = UUID.randomUUID();
+        return userTokenClient.generateSystemToken()
+                .flatMap(systemToken -> webClient.post()
+                        .uri("/api/v1/internal/transfer/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + systemToken)
+                        .header("Idempotency-Key", idempotencyKey == null ? transactionId.toString() : idempotencyKey)
+                        .bodyValue(Map.of(
+                                "transferId", transactionId,
+                                "debitAccount", request.debitAccount(),
+                                "creditAccount", request.creditAccount(),
+                                "amount", request.amount()
+                                ))
+                        .exchangeToMono(response -> webClientResponseHandler
+                                .response(response, Map.class, "createTransfer")
+                                .map(raw -> (Map<String, String>) raw))
+                        .timeout(Duration.ofSeconds(5))
+                );
+    }
 }
