4d41c2f246012917add8df39641084ce4b3267a5
Artyom <aroyanart98@gmail.com>
2025-09-08 23:36:53 +0400

    feat(account):
    - add `AccountsCommandsConsumer` class.

---
+++ services/bank-account/src/main/java/am/banking/system/account/infrastructure/adapter/in/kafka/AccountsCommandsConsumer.java
@@ -0,0 +1,119 @@
+package am.banking.system.account.infrastructure.adapter.in.kafka;
+
+import am.banking.system.account.domain.repository.AccountRepository;
+import am.banking.system.account.outbox.OutboxEvent;
+import am.banking.system.account.outbox.OutboxRepository;
+import am.banking.system.common.messages.FundsInsufficientEvent;
+import am.banking.system.common.messages.FundsReservedEvent;
+import am.banking.system.common.messages.ValidateAndReserveCommand;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import reactor.kafka.receiver.KafkaReceiver;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 08.09.25
+ * Time: 23:01:43
+ */
+@Component
+@RequiredArgsConstructor
+public class AccountsCommandsConsumer {
+    private final ObjectMapper objectMapper;
+    private final OutboxRepository outboxRepository;
+    private final AccountRepository accountRepository;
+    private final KafkaReceiver<String, String> kafkaReceiver;
+
+    public void start() {
+        kafkaReceiver.receive()
+                .flatMap(record -> {
+                    try {
+                        ValidateAndReserveCommand command = objectMapper.readValue(record.value(), ValidateAndReserveCommand.class);
+                        return handleValidateAndReserve(command)
+                                .then(Mono.fromRunnable(record.receiverOffset()::acknowledge));
+                    } catch (Exception ex) {
+                        return Mono.error(ex);
+                    }
+                })
+                .subscribe();
+    }
+
+    private Mono<Void> handleValidateAndReserve(ValidateAndReserveCommand command) {
+        return accountRepository.findAccountByAccountNumber(command.fromAccount())
+                .flatMap(acc -> {
+                    var available = acc.getBalance().subtract(acc.getReserved());
+                    if (available.compareTo(command.amount()) >= 0) {
+                        acc.toBuilder().reserved(acc.getReserved().add(command.amount())).build();
+
+                        return accountRepository.save(acc)
+                                .flatMap(_ -> {
+                                    var event = new FundsReservedEvent(
+                                            command.transferId(),
+                                            UUID.randomUUID().toString(),
+                                            command.fromAccount(),
+                                            command.amount(),
+                                            Instant.now());
+
+                                    try {
+                                        var outbox = new OutboxEvent();
+                                        outbox.setId(UUID.randomUUID());
+                                        outbox.setTopic("accounts.events");
+                                        outbox.setKey(command.fromAccount());
+                                        outbox.setPayload(objectMapper.writeValueAsString(event));
+                                        outbox.setPublished(false);
+                                        outbox.setCreatedAt(Instant.now());
+                                        return outboxRepository.save(outbox).then();
+                                    } catch (Exception ex) {
+                                        return Mono.error(ex);
+                                    }
+                                });
+                    } else {
+                        var event = new FundsInsufficientEvent(
+                                command.transferId(),
+                                command.fromAccount(),
+                                command.amount(),
+                                "INSUFFICIENT_FUNDS",
+                                Instant.now()
+                        );
+
+                        try {
+                            var outbox = new OutboxEvent();
+                            outbox.setId(UUID.randomUUID());
+                            outbox.setTopic("accounts.events");
+                            outbox.setKey(command.fromAccount());
+                            outbox.setPayload(objectMapper.writeValueAsString(event));
+                            outbox.setPublished(false);
+                            outbox.setCreatedAt(Instant.now());
+                            return outboxRepository.save(outbox).then();
+                        } catch (Exception ex) {
+                            return Mono.error(ex);
+                        }
+                    }
+                })
+                .switchIfEmpty(Mono.defer(() -> {
+                    try {
+                        var event = new FundsInsufficientEvent(
+                                command.transferId(),
+                                command.fromAccount(),
+                                command.amount(),
+                                "ACCOUNT_NOT_FOUND",
+                                Instant.now()
+                        );
+                        var outbox = new OutboxEvent();
+                        outbox.setId(UUID.randomUUID());
+                        outbox.setTopic("accounts.events");
+                        outbox.setKey(command.fromAccount());
+                        outbox.setPayload(objectMapper.writeValueAsString(event));
+                        outbox.setPublished(false);
+                        outbox.setCreatedAt(Instant.now());
+                        return outboxRepository.save(outbox).then();
+                    } catch (Exception ex) {
+                        return Mono.error(ex);
+                    }
+                }));
+    }
+}
