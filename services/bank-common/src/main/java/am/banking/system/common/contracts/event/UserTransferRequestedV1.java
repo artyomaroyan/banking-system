24ec026a8d2f44da380eecae7aa7f4779ab62c36
Artyom <aroyanart98@gmail.com>
2025-09-09 19:36:09 +0400

    refactor(common): temporary comment `UserTransferRequestedV1` record.

--- services/bank-common/src/main/java/am/banking/system/common/contracts/event/UserTransferRequestedV1.java
+++ services/bank-common/src/main/java/am/banking/system/common/contracts/event/UserTransferRequestedV1.java
@@ -1,20 +1,20 @@
-package am.banking.system.common.contracts.event;
-
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import org.springframework.validation.annotation.Validated;
-
-import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.UUID;
-
-/**
- * Author: Artyom Aroyan
- * Date: 26.08.25
- * Time: 02:27:42
- */
-@Validated
-public record UserTransferRequestedV1(@NotNull UUID eventId, @NotNull UUID userId, @NotNull UUID debitAccountId,
-                                      @NotNull UUID creditAccountId, @NotNull @Positive BigDecimal amount,
-                                      @NotNull Instant occurredAt) implements DomainEvent {
-}
+//package am.banking.system.common.contracts.event;
+//
+//import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.Positive;
+//import org.springframework.validation.annotation.Validated;
+//
+//import java.math.BigDecimal;
+//import java.time.Instant;
+//import java.util.UUID;
+//
+//**
+// * Author: Artyom Aroyan
+// * Date: 26.08.25
+// * Time: 02:27:42
+// */
+//@Validated
+//public record UserTransferRequestedV1(@NotNull UUID eventId, @NotNull UUID userId, @NotNull UUID debitAccountId,
+//                                      @NotNull UUID creditAccountId, @NotNull @Positive BigDecimal amount,
+//                                      @NotNull Instant occurredAt) implements DomainEvent {
+//}
