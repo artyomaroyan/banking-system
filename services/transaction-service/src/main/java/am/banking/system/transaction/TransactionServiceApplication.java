f2ec516349aa7ff94a127c86562614cd4dfe9ec4
Artyom <aroyanart98@gmail.com>
2025-09-20 23:12:03 +0400

    fix(transaction): trying to fix logging error

--- services/transaction-service/src/main/java/am/banking/system/transaction/TransactionServiceApplication.java
+++ services/transaction-service/src/main/java/am/banking/system/transaction/TransactionServiceApplication.java
@@ -1,9 +1,12 @@
 package am.banking.system.transaction;
 
+import am.banking.system.common.configuration.KafkaReactiveConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = {"am.banking.system.transaction", "am.banking.system.common"})
+@SpringBootApplication
+@Import(KafkaReactiveConfiguration.class)
 public class TransactionServiceApplication {
 
     public static void main(String[] args) {
