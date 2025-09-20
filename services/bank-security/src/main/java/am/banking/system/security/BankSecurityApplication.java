55ee054c0fa3a9c6f79eb55c4ed1c0375b4478b6
Artyom <aroyanart98@gmail.com>
2025-09-20 23:11:10 +0400

    fix(security): trying to fix logging error

--- services/bank-security/src/main/java/am/banking/system/security/BankSecurityApplication.java
+++ services/bank-security/src/main/java/am/banking/system/security/BankSecurityApplication.java
@@ -1,11 +1,14 @@
 package am.banking.system.security;
 
+import am.banking.system.common.infrastructure.configuration.InternalSecretProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(scanBasePackages = {"am.banking.system.security", "am.banking.system.common"})
+@SpringBootApplication
+@EnableConfigurationProperties(InternalSecretProperties.class)
 public class BankSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankSecurityApplication.class, args);
